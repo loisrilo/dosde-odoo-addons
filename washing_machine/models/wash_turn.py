96aefd98ee50c75fb3b1c145c3f867d698c8c073
Lois Rilo <lois11ra@gmail.com>
2018-06-08 19:27:10 +0200

    [IMP] washing_machine:  * fix order in tree view.  * add calendar view  * add _check_date method

--- washing_machine/models/wash_turn.py
+++ washing_machine/models/wash_turn.py
@@ -1,10 +1,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class WashTurn(models.Model):
     _name = "wash.turn"
+    _order = "date desc"
 
     user_id = fields.Many2one(
         comodel_name="res.users",
@@ -18,3 +20,10 @@ class WashTurn(models.Model):
     _sql_constraints = [
         ('date_uniq', 'unique (date)', "Date unavailable"),
     ]
+
+    @api.constrains("date")
+    def _check_date(self):
+        for rec in self:
+            if rec.date < fields.Date.today():
+                raise ValidationError(_(
+                    "You cannot travel to the past, do you?"))
