2bd89131b337b92dec39a8c9a3fb3eb4d0068f11
Lois Rilo Antelo <lois11ra@gmail.com>
2018-06-21 23:13:19 +0200

    [IMP] washing_machine:  * add form and pivot view.  * enhance user experience in responsive devices.  * allow to reschedule own turns

--- washing_machine/models/wash_turn.py
+++ washing_machine/models/wash_turn.py
@@ -7,6 +7,7 @@ from odoo.exceptions import ValidationError
 class WashTurn(models.Model):
     _name = "wash.turn"
     _order = "date desc"
+    _rec_name = 'date'
 
     user_id = fields.Many2one(
         comodel_name="res.users",
@@ -16,6 +17,7 @@ class WashTurn(models.Model):
     )
     date = fields.Date(required=True)
     notes = fields.Html()
+    past = fields.Boolean(compute='_compute_past')
 
     _sql_constraints = [
         ('date_uniq', 'unique (date)', "Date unavailable"),
@@ -27,3 +29,20 @@ class WashTurn(models.Model):
             if rec.date < fields.Date.today():
                 raise ValidationError(_(
                     "You cannot travel to the past, do you?"))
+
+    @api.multi
+    def _compute_past(self):
+        today = fields.Date.today()
+        for rec in self:
+            rec.past = rec.date < today
+
+    @api.multi
+    def write(self, vals):
+        user = self.env.user
+        if (not user.has_group('washing_machine.group_washing_machine_manager')
+                and (self.mapped('user_id') != user or
+                     any(self.mapped('past')))):
+            raise ValidationError(_(
+                "You are not allowed to write other user's turns or "
+                "past turns."))
+        return super().write(vals)
