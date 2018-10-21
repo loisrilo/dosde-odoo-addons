be6de3ad40191177f4fbcc17533c4ca2e612fa3a
Lois Rilo Antelo <lois11ra@gmail.com>
2018-10-21 23:12:01 +0200

    [11.0][ADD] housekeeping

---
+++ housekeeping/README.rst
@@ -0,0 +1,18 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+============
+Housekeeping
+============
+
+Manage the housekeeping task:
+
+* Define tasks.
+* Auto-assign them periodically.
+* Send e-mail reminders.
+
+Credits
+=======
+
+* Lois Rilo <github.com/loisrilo>
---
+++ housekeeping/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Housekeeping",
+    "summary": "Manage Housekeeping tasks.",
+    "version": "11.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://github.com/loisrilo/",
+    "author": "Lois Rilo",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "security/housekeeping_security.xml",
+        "security/ir.model.access.csv",
+        "data/housekeeping_data.xml",
+        "views/housekeeping_view.xml",
+        "views/house_task_view.xml",
+    ],
+}
