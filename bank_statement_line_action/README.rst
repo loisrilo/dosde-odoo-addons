612f53d4cd96cef39623b47c7b9ea33dffadbbb4
Lois Rilo Antelo <lois11ra@gmail.com>
2019-09-21 18:43:44 +0200

    [12.0][ADD] bank_statement_line_action

---
+++ bank_statement_line_action/README.rst
@@ -0,0 +1,15 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+==========================
+Bank Statement Line Action
+==========================
+
+Adds bank statement line action to access tree view and be able to
+easily import them.
+
+Credits
+=======
+
+* Lois Rilo <github.com/loisrilo>
---
+++ bank_statement_line_action/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Statement Line Action",
+    "summary": "Adds bank statement line action to access tree view and be "
+               "able to easily import them.",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/loisrilo/",
+    "author": "Lois Rilo",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_bank_statement_view.xml",
+    ],
+}
