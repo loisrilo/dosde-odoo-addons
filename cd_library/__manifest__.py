798190870e89561fa78f6d05dbe5c53eb88dd428
Lois Rilo <lois11ra@gmail.com>
2022-11-14 20:27:30 +0100

    [14.0][MIG] cd_library

--- cd_library/__manifest__.py
+++ cd_library/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CD Library",
     "summary": "Manage a simple registry of music disks.",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/loisrilo/",
     "author": "Lois Rilo",
