cc5c5ca20716643e7d2b07fca6b224eebcb937aa
Lois Rilo <lois11ra@gmail.com>
2023-01-15 18:52:37 +0100

    [IMP] music_library: download music as a zip file.

--- music_library/models/music_track.py
+++ music_library/models/music_track.py
@@ -1,6 +1,9 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+
+import zipfile
+from io import BytesIO
 
 
 class MusicTrack(models.Model):
@@ -35,4 +38,31 @@ class MusicTrack(models.Model):
         for rec in self:
             if not rec.name or not rec.artist_id:
                 continue
-            rec.filename = "%s - %s" % (rec.artist_id.name, rec.name)
+            rec.filename = "%s - %s.mp3" % (rec.artist_id.name, rec.name)
+
+    def action_music_zip_download(self):
+        ids = ",".join(map(str, self.ids))
+        return {
+            "type": "ir.actions.act_url",
+            "url": "/web/music_track/download_zip?ids=%s" % (ids),
+            "target": "self",
+        }
+
+    def _create_temp_zip(self):
+        zip_buffer = BytesIO()
+        domain = [
+            ("res_model", "=", self._name),
+            ("res_field", "=", "file"),
+            ("res_id", "in", self.ids),
+        ]
+        attachments = self.env["ir.attachment"].sudo().search(domain)
+
+        with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+            for rec in self:
+                attachment = attachments.filtered(lambda a: a.res_id == rec.id)
+                zip_file.write(
+                    attachment._full_path(attachment.store_fname), rec.filename
+                )
+            zip_buffer.seek(0)
+            zip_file.close()
+        return zip_buffer
