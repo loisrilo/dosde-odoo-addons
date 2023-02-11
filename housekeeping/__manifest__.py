61f37da442d1ace5957788fc67dfe83b76e0445f
Lois Rilo <lois11ra@gmail.com>
2023-02-11 10:22:40 +0100

    [MIG] housekeeping: Migration to 15.0

--- housekeeping/__manifest__.py
+++ housekeeping/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Housekeeping",
     "summary": "Manage Housekeeping tasks.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/loisrilo/",
     "author": "Lois Rilo",
