e5c92669ce7f5dd7ddcedec97dca4344ed3f94b0
Lois Rilo <lois11ra@gmail.com>
2026-07-13 18:53:50 +0200

    [MIG] home_library: Migration to 18.0

--- home_library/__manifest__.py
+++ home_library/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Home Library",
     "summary": "Manage a simple registry of books.",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/loisrilo/dosde-odoo-addons",
     "author": "Lois Rilo",
