79b3375a29f33b704cf1b2dcee37b92d89d58c10
Lois Rilo <lois11ra@gmail.com>
2026-07-13 18:43:51 +0200

    [MIG] money_pot: Migration to 18.0

--- money_pot/__manifest__.py
+++ money_pot/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Money Pot",
     "summary": "Money Pot to share expenses.",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/loisrilo/dosde-odoo-addons",
     "author": "Lois Rilo",
